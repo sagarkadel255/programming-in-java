5f5bbf8340395134f6d37e7d3c8b67a42a0c6095
sagar <149937501+sagarkadel255@users.noreply.github.com>
2024-04-30 18:25:06 +0545

    Add files via upload

---
+++ escapesequences.java
@@ -0,0 +1,12 @@
+public class escapesequences {
+    
+    public static void main(String[] args) {
+        System.out.println("A \"quoted\" String is\n"
+                         + "'much' better if you learn\n"
+                         + "the rules of \"escape sequences.\"\n"
+                         + "Also, \"\" represents an empty\n"
+                         + "String. Don't forget: use \\\" \n"
+                         + "instead of \" !\n"
+                         + "'' is not the same as \"");
+    }
+}
